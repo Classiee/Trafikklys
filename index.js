baae6af61e241b96567291df069919b42a885369
Classiee <Kristianfratini@gmail.com>
2024-08-15 13:08:48 +0200

    Rydda opp kode

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function activeOrange (){
-    activeLight('rød','grønn','orange','orange', 2000, activeGreen);
+function activeOrange() {
+  activeLight("rød", "grønn", "orange", "orange", 2000, activeGreen);
 }
 
-function activeGreen (){
-    activeLight('rød','orange','grønn','grønn', 2000, activeRed);
+function activeGreen() {
+  activeLight("rød", "orange", "grønn", "grønn", 2000, activeRed);
 }
 
-function activeLight(divId1, divId2, divId3, divId4, delay, nextLightFunction){
-    document.getElementById(divId1).classList.remove('active');
-    document.getElementById(divId2).classList.remove('active');
-    document.getElementById(divId3).classList.remove('active');
-    document.getElementById(divId4).classList.add('active');
+function activeLight(divId1, divId2, divId3, divId4, delay, nextLightFunction) {
+  document.getElementById(divId1).classList.remove("active");
+  document.getElementById(divId2).classList.remove("active");
+  document.getElementById(divId3).classList.remove("active");
+  document.getElementById(divId4).classList.add("active");
 
-    setTimeout(nextLightFunction, delay);
+  setTimeout(nextLightFunction, delay);
 }
 
 function activeRed() {
-    activeLight('grønn','orange','rød','rød', 2000, activeOrange);
+  activeLight("grønn", "orange", "rød", "rød", 2000, activeOrange);
 }
 
 activeRed();

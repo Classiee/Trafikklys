d75ca00e5790ba3033342fec7a69442fe59d9031
Classiee <Kristianfratini@gmail.com>
2024-08-15 12:44:14 +0200

    Trafikklys oppdatering
    JS oppdatert

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function activeOrange (){
-    activeLight('rød','grønn','orange','orange');
+    activeLight('rød','grønn','orange','orange', 2000, activeGreen);
 }
+
+function activeGreen (){
+    activeLight('rød','orange','grønn','grønn', 2000, activeRed);
+}
+
+function activeLight(divId1, divId2, divId3, divId4, delay, nextLightFunction){
+    document.getElementById(divId1).classList.remove('active');
+    document.getElementById(divId2).classList.remove('active');
+    document.getElementById(divId3).classList.remove('active');
+    document.getElementById(divId4).classList.add('active');
+
+    setTimeout(nextLightFunction, delay);
+}
+
+function activeRed() {
+    activeLight('grønn','orange','rød','rød', 2000, activeOrange);
+}
+
+activeRed();
